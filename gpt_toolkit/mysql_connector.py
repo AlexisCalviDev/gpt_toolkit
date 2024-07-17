477e1db0835710f134e4e7d8f37673b9ca6f17d0
alcal <alexis.calvi@lyaprotect.com>
2024-07-17 19:41:53 +0200

    mysql_connector

--- gpt_toolkit/mysql_connector.py
+++ gpt_toolkit/mysql_connector.py
@@ -6,19 +6,24 @@ class MySQLConnector:
 
     def __init__(self,
                  sql_hostname,
-                 sql_port):
+                 sql_port,
+                 sql_username,
+                 sql_password,
+                 sql_database
+                 ):
         self.sql_hostname = sql_hostname
         self.sql_port = sql_port
+        self.sql_username = sql_username
+        self.sql_password = sql_password
+        self.sql_database = sql_database
 
-    def connect(self, sql_username,
-                sql_password,
-                sql_main_database):
+    def connect(self):
         self.conn = pymysql.connect(
             host=self.sql_hostname,
-            user=sql_username,
-            passwd=sql_password,
+            user=self.sql_username,
+            passwd=self.sql_password,
             port=self.sql_port,
-            database=sql_main_database
+            database=self.sql_database
         )
         print("Connected to MySQL database")
         return self.conn
@@ -32,7 +37,7 @@ class MySQLConnector:
         data = pd.read_sql_query(query, self.conn)
         return data
 
-    def df_to_mysql(self, df, table_name, if_exists='replace'):
+    def df_to_mysql(self, df, table_name, if_exists='append'):
         conn_str = f'mysql+pymysql://{self.conn.user}:{self.conn.password}@{self.sql_hostname}:{self.sql_port}/{self.conn.db}'
         engine = create_engine(conn_str)
         df.to_sql(name=table_name, con=engine, if_exists=if_exists, index=False)
